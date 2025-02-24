d6ad234d7e351a31561d6dd5f8462c673aca949a
Samuel Quiroz <Samuel.Quiroz@teaminternational.com>
2025-02-24 11:39:20 -0500

    Se corrige el setup para que se instalen los paquetes necesarios

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name ='catlibrary',
-    version='1.2',
+    version='1.3',
     packages=find_packages(),
     description='Libreria para obtener gatos',
     long_description='Hola',
     long_description_content_type='text',
-    author='Samuel',
+    author='Samuel Quiroz',
     author_email='prueba@gmail.com',
-    url='probando',
-    requires=['requests'],
+    url='https://github.com/Samuzarter/CATLIBRARY.git',
+    install_requires=['requests','python-decouple','python-dotenv']
 )
